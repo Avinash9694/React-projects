e2132ded41fcf31858b9c081fcbdb70b6e942e55
Avinash Sahu <96678635+Avinash9694@users.noreply.github.com>
2024-01-04 22:00:26 +0530

    added useeffect fetch data project

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import Counter from './components/useState/Counter'
 import Final from './components/useState/Final'
 import ObjectExample from './components/useState/ObjectExample'
+import FetchData from './components/useEffect/FetchData'
 
 function App() {
   return (
     <>
       <Counter/>
       {/* <Final/> */}
+      <FetchData/>
       <ObjectExample/>
     </>
   )

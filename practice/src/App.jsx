29aa6d0e68c77e082683d1180572717d30e4c14b
Avinash Sahu <96678635+Avinash9694@users.noreply.github.com>
2024-01-09 21:12:54 +0530

    added custom fetch user project

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,29 +1,34 @@
-import './App.css';
-import Counter from './components/useState/Counter';
-import Final from './components/useState/Final';
-import ObjectExample from './components/useState/ObjectExample';
-import FetchData from './components/useEffect/FetchData';
-import FetchUser from './components/useEffect/FetchUser';
-import ShortCircuit from './components/useEffect/ShortCircuit';
-import UserLogin from './components/useEffect/UserLogin';
-import Timer from './components/useEffect/Timer';
-import Tour from './components/useEffect/Tour';
-import ColourGenerator from './components/useEffect/ColourGenerator';
+import "./App.css";
+import Counter from "./components/useState/Counter";
+import Final from "./components/useState/Final";
+import ObjectExample from "./components/useState/ObjectExample";
+import FetchData from "./components/useEffect/FetchData";
+import FetchUser from "./components/useEffect/FetchUser";
+import ShortCircuit from "./components/useEffect/ShortCircuit";
+import UserLogin from "./components/useEffect/UserLogin";
+import Timer from "./components/useEffect/Timer";
+import Tour from "./components/useEffect/Tour";
+import ColourGenerator from "./components/useEffect/ColourGenerator";
+import Toggle from "./components/customHook/Toggle";
+import FetchCustomData from "./components/customHook/FetchCustomData";
+
 function App() {
   return (
     <>
-      <Counter/>
+      {/* <Counter /> */}
+      {/* <Toggle /> */}
       {/* <Final/> */}
       {/* <FetchData/> */}
       {/* <FetchUser/> */}
       {/* <ObjectExample/> */}
-      <ColourGenerator/>
+      {/* <ColourGenerator/>
       <Tour/>
       <ShortCircuit/>
       <UserLogin/>
-      <Timer/>
+      <Timer/> */}
+      <FetchCustomData />
     </>
-  )
+  );
 }
 
-export default App
+export default App;

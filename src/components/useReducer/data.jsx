20cfbe8072a704bd39a1ee709cf5a01b6720b772
Avinash Sahu <96678635+Avinash9694@users.noreply.github.com>
2024-01-13 16:49:43 +0530

    Delete unnecessary files

--- src/components/useReducer/data.jsx
+++
@@ -1,6 +0,0 @@
-export const Person = [
-  { id: 1, name: "Ramesh", age: 25, hobby: "Reading books" },
-  { id: 2, name: "Suresh", age: 26, hobby: "Playing cricket" },
-  { id: 3, name: "John", age: 27, hobby: "Watching movies" },
-  { id: 4, name: "Paul", age: 28, hobby: "Travelling" },
-];

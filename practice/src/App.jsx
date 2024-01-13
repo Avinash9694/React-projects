184c933d7cc0e14a315b39c08a043125e306fd6a
Avinash Sahu <96678635+Avinash9694@users.noreply.github.com>
2024-01-13 16:27:01 +0530

    added reducer and css file

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -14,6 +14,7 @@ import "./App.css";
 // import Navbar from "./components/contextApi/Navbar";
 // import { useGlobalContext } from "./components/contextApi/context";
 // import Stripe from "./components/contextApi/stripe-project/Stripe";
+import Cart from "./components/useReducer/Cart-project/Cart";
 import Reducer from "./components/useReducer/Reducer";
 function App() {
   // const { name } = useGlobalContext();
@@ -34,7 +35,8 @@ function App() {
       {/* <FetchCustomData /> */}
       {/* <Navbar /> */}
       {/* <Stripe /> */}
-      <Reducer />
+      {/* <Reducer /> */}
+      <Cart />
     </>
   );
 }
--- practice/src/main.jsx
+++ practice/src/main.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import AppContext from "./components/contextApi/context.jsx";
+// import AppContext from "./components/contextApi/context.jsx";
 // import { AppProvider } from "./components/contextApi/stripe-project/context.jsx";
+import { AppProvider } from "./components/useReducer/Cart-project/context.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <AppProvider> */}
-    {/* <AppContext> */}
-    <App />
-    {/* </AppContext> */}
-    {/* </AppProvider> */}
+    <AppProvider>
+      {/* <AppContext> */}
+      {/* <AppContext1> */}
+      <App />
+      {/* </AppContext1> */}
+      {/* </AppContext> */}
+    </AppProvider>
   </React.StrictMode>
 );

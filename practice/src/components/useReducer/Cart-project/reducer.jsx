6842d5dca8e277d5b712f72056335836d4e8ad61
Avinash Sahu <96678635+Avinash9694@users.noreply.github.com>
2024-01-13 18:13:03 +0530

    increase and decrease functionality are added

--- practice/src/components/useReducer/Cart-project/reducer.jsx
+++ practice/src/components/useReducer/Cart-project/reducer.jsx
@@ -6,6 +6,28 @@ const reducer = (state, action) => {
     const newCart = state.cart.filter((item) => item.id !== action.payload);
     return { ...state, cart: newCart };
   }
+
+  if (action.type === "INCREASE") {
+    const currentAmount = state.cart.map((item) => {
+      if (item.id === action.payload) {
+        return { ...item, amount: item.amount + 1 };
+      }
+      return item;
+    });
+    return { state, cart: currentAmount };
+  }
+
+  if (action.type === "DECREASE") {
+    const tempItem = state.cart
+      .map((item) => {
+        if (item.id === action.payload) {
+          return { ...item, amount: item.amount - 1 };
+        }
+        return item;
+      })
+      .filter((cartItem) => cartItem.amount !== 0);
+    return { ...state, cart: tempItem };
+  }
   return state;
 };
 
